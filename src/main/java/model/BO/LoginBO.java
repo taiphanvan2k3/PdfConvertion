8dcc38c7af64df00f635df74ec262d89002bf739
taiphanvan2k3 <taiphanvan2403@gmail.com>
2023-12-08 09:15:33 +0700

    feat: Implement convert pdf to docx

---
+++ src/main/java/model/BO/LoginBO.java
@@ -0,0 +1,5 @@
+package model.BO;
+
+public class LoginBO {
+
+}
---
+++ src/main/java/utils/Utils.java
@@ -0,0 +1,43 @@
+package utils;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Utils {
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/online-exam", "root", "");
+		} catch (Exception e) {
+			System.out.println("Fail");
+		}
+		return conn;
+	}
+
+	public static void redirectToPage(HttpServletRequest request, HttpServletResponse response, String destination) {
+		try {
+			response.sendRedirect(request.getContextPath() + destination);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Date convertStringToDate(String dateString) {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+		Date date = new Date();
+		try {
+			date = formatter.parse(dateString);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return date;
+	}
+}
