2c230eb92c60e0eaf9b9b3a8ade5841c01614a76
taiphanvan2k3 <taiphanvan2403@gmail.com>
2023-12-11 20:54:22 +0700

    fix: Rename controller

--- src/main/java/controller/DownloadFileServlet.java
+++ src/main/java/controller/DownloadFileServlet.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,29 +8,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import model.BEAN.Upload;
-import model.BO.ConverterBO;
 import utils.Utils;
 
-@WebServlet("/ListConvertController")
-public class ListConvertController extends HttpServlet {
+@WebServlet("/DownloadFileServlet")
+public class DownloadFileServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public ListConvertController() {
+	public DownloadFileServlet() {
 		super();
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		ArrayList<Upload> uploads = (new ConverterBO())
-				.getListFileConvert((String) request.getSession().getAttribute("username"));
-		request.getSession().setAttribute("uploads", uploads);
-		Utils.redirectToPage(request, response, "/viewListConvert.jsp");
+		String action = (String) request.getParameter("action");
+		if (action.equals("downloadfile")) {
+			String fileName = request.getParameter("fileName");
+			Utils.downloadFile(request, response, fileName);
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
 	}
-
 }
--- src/main/java/controller/ListConvertServlet.java
+++ src/main/java/controller/ListConvertServlet.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,27 +9,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.BEAN.Upload;
+import model.BO.ConverterBO;
 import utils.Utils;
 
-@WebServlet("/DownloadFileController")
-public class DownloadFileController extends HttpServlet {
+@WebServlet("/ListConvertServlet")
+public class ListConvertServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public DownloadFileController() {
+	public ListConvertServlet() {
 		super();
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String action = (String) request.getParameter("action");
-		if (action.equals("downloadfile")) {
-			String fileName = request.getParameter("fileName");
-			Utils.downloadFile(request, response, fileName);
-		}
+		ArrayList<Upload> uploads = (new ConverterBO())
+				.getListFileConvert((String) request.getSession().getAttribute("username"));
+		request.getSession().setAttribute("uploads", uploads);
+		Utils.redirectToPage(request, response, "/viewListConvert.jsp");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
 	}
+
 }
